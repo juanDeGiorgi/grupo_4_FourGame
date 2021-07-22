818dbd5a1c0def147fea1d0da5674f72f9177602
gonzazorzal <gonzalo220201@gmail.com>
2021-07-22 19:10:10 -0300

    add partials routing

--- sprint3/app.js
+++ sprint3/app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');
+const cartRouter = require('./routes/cart');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/cart', cartRouter);
 
 /*Pendiente de RUTEO*/
 
--- sprint3/routes/users.js
+++ sprint3/routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const controller = require('../controllers/usersController'); 
+
+router.get('/register', controller.register);
+router.get('/login', controller.login);
 
 module.exports = router;

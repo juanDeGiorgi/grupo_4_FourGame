de0351de6cb718b357edc5b8cc82c5dabc7548df
juanDeGiorgi <juanmadegiorgi@gmail.com>
2021-08-04 15:23:46 -0300

    add preliminary products json

--- site/controllers/productsController.js
+++ site/controllers/productsController.js
@@ -5,4 +5,8 @@ module.exports={
     detail : (req,res)=> res.render('detailProduct'),
 
     loading : (req,res)=> res.render('productLoading'),
+
+    edit : (req,res)=> res.render('productEdit'),
+
 }
+
--- site/routes/products.js
+++ site/routes/products.js
@@ -5,6 +5,8 @@ const controller = require("../controllers/productsController");
 
 router.get('/detail-product', controller.detail);
 router.get('/product-loading', controller.loading);
+router.get('/product-edit', controller.edit);
+
 
 module.exports = router;
 

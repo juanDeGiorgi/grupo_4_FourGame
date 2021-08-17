08da7a4287f5173daea37dfa957165bd9d297925
ezequielrango <ezequielrango@hotmail.com>
2021-08-17 12:51:54 -0300

    require session and apply in the app.js

--- site/app.js
+++ site/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const methodOverride = require("method-override");
+const session = require('express-session');
 
 // import routers
 const indexRouter = require('./routes/index');
@@ -24,7 +25,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride("_method"));
+app.use(session({
+  secret : "my secret",
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(cookieCheck);
+app.use(localsUserCheck);
 
 // config routes
 app.use('/',indexRouter);

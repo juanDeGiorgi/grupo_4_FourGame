d19bc48ad9bb7390e663238ca07c3121d28b6447
ezequielrango <ezequielrango@hotmail.com>
2021-08-17 13:17:48 -0300

    add register validations

---
+++ site/validations/registerValidator.js
@@ -0,0 +1,36 @@
+const {check, body} = require('express-validator');
+
+module.exports = [
+
+    check('name')
+    .notEmpty().withMessage('El nombre es obligatorio').bail()
+    .isLength({
+        min : 2,
+        max : 50
+    }).withMessage('El nombre tiene que tener como mínimo 2 caracteres').bail()
+    .isAlpha().withMessage('El nombre debe contener solo letras'),
+
+
+    check('email')
+    .isEmail().withMessage('Debes ingresar un email válido'),
+
+
+    check('password')
+    .isLength({
+        min : 6,
+        max : 12
+    }).withMessage('La contraseña debe tener entre 6 y 12 caracteres'),
+
+
+    body('password2')
+    .custom((value,{req}) => {
+        if(value !== req.body.password){
+            return false
+        }
+        return true
+    }).withMessage('Las contraseñas no coinciden'),
+
+    
+    check('acepta')
+    .isString('on').withMessage('Debes aceptar los términos y condiciones')
+]

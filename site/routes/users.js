bbd3267cec84c0e1233d3bbd4b110c67a6a78fd6
ezequielrango <ezequielrango@hotmail.com>
2021-08-23 00:34:03 -0300

    add registerProcess route

--- site/routes/users.js
+++ site/routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
 const controller = require('../controllers/usersController'); 
 
 router.get('/register', controller.register);
+router.post('/register',controller.registerProcess);
 router.get('/login', controller.login);
 
 module.exports = router;

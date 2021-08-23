09323873fdff156b1e3c1ec23bd3863866e822d7
ezequielrango <ezequielrango@hotmail.com>
2021-08-23 01:10:05 -0300

    require validation in the route

--- site/routes/users.js
+++ site/routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/usersController'); 
+const validation = require("../validations/registerValidator")
 
 router.get('/register', controller.register);
-router.post('/register',controller.registerProcess);
+router.post('/register',validation,controller.registerProcess);
 router.get('/login', controller.login);
 
 module.exports = router;

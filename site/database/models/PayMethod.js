3a32aea3d8a5c50405ff19b253ef0522d6b31832
ezequielrango <ezequielrango@hotmail.com>
2021-09-14 13:50:19 -0300

    create PayMethod model

---
+++ site/database/models/PayMethod.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize,dataTypes) => {
+
+    const alias = "payMethods"; 
+
+    const cols = {
+       
+        id: {
+            type: dataTypes.INTEGER,
+            autoIncrement : true,
+            primaryKey: true,
+            allowNull: false
+        },
+
+        name: {
+            type:dataTypes.STRING(255),
+            allowNull: false,
+        },
+
+    }
+
+    const config = {
+        timestamps = true,
+        tableName= "payMethods",
+    }
+
+    const PayMethod = sequelize.define(alias,cols,config);
+
+    return PayMethod; 
+
+}

bcac5b5875d7bce203a65d02d92c426fdc532d0b
ezequielrango <ezequielrango@hotmail.com>
2021-09-14 02:53:59 -0300

    create product model

---
+++ site/database/models/Product.js
@@ -0,0 +1,73 @@
+module.exports = (sequelize,dataTypes) => {
+
+    const alias = "products"; 
+
+    const cols = {
+       
+        id: {
+            type: dataTypes.INTEGER,
+            autoIncrement : true,
+            primaryKey: true,
+            allowNull: false
+        },
+
+        name: {
+            type: dataTypes.STRING(255),
+            allowNull: false,
+        },
+
+        price: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+        },
+
+        discount: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+            defaultValue: 0,
+        },
+
+        description: {
+            type: dataTypes.STRING(500),
+            allowNull: false,
+        },
+
+        deletedAt: {
+            type:dataTypes.DATE,
+        },
+
+        delete: {
+            type: dataTypes.INTEGER(1),
+            allowNull: false,
+            defaultValue: 0,
+        },
+
+        categoryId: {
+            type : dataTypes.INTEGER,
+            allowNull:false,
+        },
+
+        userId: {
+            type : dataTypes.INTEGER,
+            allowNull:false,
+        },
+
+        typeProductId: {
+            type : dataTypes.INTEGER,
+            allowNull:false,
+        },
+
+
+
+    }
+
+    const config = {
+        timestamps = true,
+        tableName= "products",
+    }
+
+    const Product = sequelize.define(alias,cols,config);
+
+    return Product ; 
+
+}

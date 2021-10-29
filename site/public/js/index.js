46e3f0b70beb6f5df6776c80ce0f48ef81d2427f
juanDeGiorgi <juanmadegiorgi@gmail.com>
2021-10-29 17:27:25 -0300

    add carrusel in index view

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,6 +1,26 @@
 
 window.addEventListener("load",() =>{
 
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        slidesPerView: 4,
+        spaceBetween: 40,
+        freeMode: true,
+        direction: 'horizontal',
+        loop: true,    
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+    });
+
     // favoritos
 
         const buttonFavorite = $("buttonFavorite");

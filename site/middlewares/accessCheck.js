a4c6dcadc080875ce220ac98ee3b2059b5e75427
juanDeGiorgi <juan.degiorgi@enviame.io>
2022-09-03 19:38:57 -0300

    dockerize application

--- site/middlewares/accessCheck.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req,res,next) =>{
-    if(req.session.userLogged && req.session.userLogged.access == 2){
-        next()
-    }else{
-        return res.redirect("/");
-    }
-    console.log(req.session.userLogged);
-}
--- site/middlewares/localsCheck.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    if(req.session.userLogged){
-        res.locals.userLogged = req.session.userLogged
-    }
-    
-    next();
-}
-  
